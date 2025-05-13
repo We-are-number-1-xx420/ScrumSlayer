b54a1af20c1d39b299505cabf6be221265e5df57
TCAGM-Dev <thomas@andringa.org>
2025-05-13 17:31:14 +0200

    Remove Renderable from Room

--- src/main/java/io/github/wearenumberone/scrumslayer/mapping/Room.java
+++ src/main/java/io/github/wearenumberone/scrumslayer/mapping/Room.java
@@ -1,9 +1,7 @@
 package io.github.wearenumberone.scrumslayer.mapping;
 
-import io.github.wearenumberone.scrumslayer.render.Renderable;
 import io.github.wearenumberone.scrumslayer.render.StyledCharacter;
-import io.github.wearenumberone.scrumslayer.util.Grid;
 
-public abstract class Room implements Renderable {
-	public abstract Grid<StyledCharacter> render();
+public abstract class Room {
+	public abstract StyledCharacter getMinimapAppearance();
 }
