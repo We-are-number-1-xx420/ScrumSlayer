925c8d7925ebbbd5f678f373832a551b04ef3956
Julius Schutte <julius.schutte@gmail.com>
2025-05-13 10:44:00 +0200

    Add ability to use position in Grid.map Credit: Thomas

--- src/main/java/io/github/wearenumberone/scrumslayer/util/Grid.java
+++ src/main/java/io/github/wearenumberone/scrumslayer/util/Grid.java
@@ -157,12 +157,19 @@ public class Grid<T> {
 		return builder.toString();
 	}
 
-	public <K> Grid<K> map(Function<T, K> mapFunction) {
+	@FunctionalInterface
+	public interface FullMapFunction<R, K> {
+		K apply(R value, Vec2i position);
+	}
+	public <K> Grid<K> map(FullMapFunction<T, K> mapFunction) {
 		Grid<K> result = new Grid<>(this.getWidth(), this.getHeight());
 		for (int y = 0; y < this.getHeight(); y++) for (int x = 0; x < this.getWidth(); x++) {
-			result.set(x, y, mapFunction.apply(this.get(x, y)));
+			result.set(x, y, mapFunction.apply(this.get(x, y), new Vec2i(x, y)));
 		}
 		return result;
+	}
+	public <K> Grid<K> map(Function<T, K> mapFunction) {
+		return this.map((value, _) -> mapFunction.apply(value));
 	}
 
 	public static Grid<Character> fromCharGrid(String charGrid) {
