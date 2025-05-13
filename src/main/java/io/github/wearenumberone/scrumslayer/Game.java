3321e3edbf57af5c8e53df561c52a4713dd6f772
Julius Schutte <julius.schutte@gmail.com>
2025-05-13 17:34:27 +0200

    Game.class Accept constant input process later

---
+++ src/main/java/io/github/wearenumberone/scrumslayer/Game.java
@@ -0,0 +1,24 @@
+package io.github.wearenumberone.scrumslayer;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class Game {
+    private InputStream in;
+    private boolean running;
+
+    public Game(InputStream in){
+        this.in = in;
+    }
+
+    public void start(){
+        final Scanner sc = new Scanner(in);
+        while(running){
+            processInput(sc.next());
+        }
+    }
+
+    public void processInput(String input){
+        //Call needed methods
+    }
+}
